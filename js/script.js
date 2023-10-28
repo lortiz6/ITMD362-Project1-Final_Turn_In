ffc13df09099ee0c827c768ceb6eebdcb0d84897
Liliana Ortiz <lilianadcortiz@gmail.com>
2023-10-27 22:47:42 -0500

    added function validateEmail(input) to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+function validateEmail(input) {
+  const email = input.value;
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const errorElement = document.getElementById('email-error');
+
+  if (!email.match(emailPattern)) {
+    errorElement.textContent = 'Please enter a valid email address';
+  } else {
+    errorElement.textContent = '';
+  }
+}
+
 const signupForm = document.getElementById('signup-form');
 signupForm.addEventListener('submit', function (event) {
     event.preventDefault();
